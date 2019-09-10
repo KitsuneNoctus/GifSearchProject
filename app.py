97db9b9c62966acdcd51d7096f66c64d6d4388a1
KitsuneNoctus <theceintiant@gmail.com>
2019-09-10 16:08:40 -0700

    Made two routes for reults and home page. Git help

--- app.py
+++ app.py
@@ -6,41 +6,37 @@ import json
 
 app = Flask(__name__)
 #Help from Kevin
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
 def index():
     """Return homepage."""
     # TODO: Extract query term from url
     #reference used https://realpython.com/api-integration-in-python/
-    query = request.form.get('Gif')
-    # if request.method == 'POST':
-    #     query = request.form.get('Gif')
+    return render_template("index.html")
     #     top_10gifs = tenorApi.findFunc(query)
     #     results = top_10gifs['results']
-
-    apiKey = "O2KCLOOCB0K2"
-    limit = 10
         #Referece to https://tenor.com/gifapi/documentation#quickstart-search
 
         # params = {
         # "query": query,
         # "apiKey": apiKey
         # }
-    gif_request = requests.get("https://api.tenor.com/v1/search?q=%s&key=%s&limit=%s" % (query, apiKey, limit))
-    if gif_request.status_code == 200:
-        # load the GIFs using the urls for the smaller GIF sizes
-        top_10gifs = json.loads(gif_request.content)
-        #Got help from a fellow student Ben for this part
-        results = top_10gifs['results']
-        #return top_10gifs
-    else:
-        top_10gifs = None
+@app.route('/results', methods=['POST'])
+def results():
+    """result."""
+    # TODO: Extract query term from url
+    #reference used https://realpython.com/api-integration-in-python/
+    query = request.form.get('Gif')
+    results = findFunc(query)
+    gif_urls = results['results']
+    return render_template("results.html", gifs=gif_urls)
+
+
+
     # #Referece to https://tenor.com/gifapi/documentation#quickstart-search
 
 
     # TODO: Make 'params' dict with query term and API key
     #Referece to https://tenor.com/gifapi/documentation#quickstart-search
-
-
 
     # # TODO: Make an API call to Tenor using the 'requests' library
     #
@@ -48,7 +44,25 @@ def index():
     #
     # # TODO: Render the 'index.html' template, passing the gifs as a named parameter
 
-    return render_template("index.html", gifs=results)
+
+
+
+def findFunc(search_term):
+    apiKey = "O2KCLOOCB0K2"
+    limit = 10
+    gif_request = requests.get("https://api.tenor.com/v1/search?q=%s&key=%s&limit=%s" % (search_term, apiKey, limit))
+    if gif_request.status_code == 200:
+        # load the GIFs using the urls for the smaller GIF sizes
+        top_10gifs = json.loads(gif_request.content)
+        #Got help from a fellow student Ben for this part
+        #results = top_10gifs['results']
+
+        #return top_10gifs
+    else:
+        top_10gifs = None
+
+    return top_10gifs
+
 
 if __name__ == '__main__':
     app.run(debug=True)
