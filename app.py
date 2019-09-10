d8ea19c2ae259f0f7ccc5d00146c4596da7f2906
KitsuneNoctus <theceintiant@gmail.com>
2019-09-10 09:45:21 -0700

    Got rid of body of tenor api stuff. Made link to other file

--- app.py
+++ app.py
@@ -1,38 +1,41 @@
 from flask import Flask, render_template, request
-import requests
+import tenorApi
 import json
 
 app = Flask(__name__)
-
-@app.route('/')
+#Help from Kevin
+@app.route('/', methods=['GET', 'POST'])
 def index():
     """Return homepage."""
     # TODO: Extract query term from url
     #reference used https://realpython.com/api-integration-in-python/
-    query = request.args.get('Gif')
+    if request.method == 'POST':
+        query = request.form.get('Gif')
+        return tenorApi.findFunc(query)
     # TODO: Make 'params' dict with query term and API key
     #Referece to https://tenor.com/gifapi/documentation#quickstart-search
-    apiKey = 	O2KCLOOCB0K2
-    limit = 10
-    #TEST search
-    search_term = "Smiling"
-    #Referece to https://tenor.com/gifapi/documentation#quickstart-search
-    #params = {"":""}
-    gif_request = requests.get("https://api.tenor.com/v1/search?q=%s&key=%s&limit=%s" % (search_term, apiKey, limit))
-    if gif_request.status_code == 200:
-        # load the GIFs using the urls for the smaller GIF sizes
-        top_10gifs = json.loads(gif_request.content)
-        print top_10gifs
-    else:
-        top_10gifs = None
-    # TODO: Make an API call to Tenor using the 'requests' library
+    # return query
+    # apiKey = 	O2KCLOOCB0K2
+    # limit = 10
+    # #TEST search
+    # search_term = "Smiling"
+    # #Referece to https://tenor.com/gifapi/documentation#quickstart-search
+    # #params = {"":""}
+    # gif_request = requests.get("https://api.tenor.com/v1/search?q=%s&key=%s&limit=%s" % (search_term, apiKey, limit))
+    # if gif_request.status_code == 200:
+    #     # load the GIFs using the urls for the smaller GIF sizes
+    #     top_10gifs = json.loads(gif_request.content)
+    #     print top_10gifs
+    # else:
+    #     top_10gifs = None
+    # # TODO: Make an API call to Tenor using the 'requests' library
+    #
+    # # TODO: Get the first 10 results from the search results
+    #
+    # # TODO: Render the 'index.html' template, passing the gifs as a named parameter
+    # named_Parameter = []
 
-    # TODO: Get the first 10 results from the search results
-
-    # TODO: Render the 'index.html' template, passing the gifs as a named parameter
-    named_Parameter = []
-
-    return render_template("index.html",top_10gifs)
+    return render_template("index.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
